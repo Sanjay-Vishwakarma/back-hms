330280901f2f15fb860e24db1fb1829bff587a06
Sanjay <sanjay.vishwakarma@ippopay.com>
2026-06-19 18:10:44 +0530

    mongo db updated : done

--- src/main/java/com/learn/jd/dto/BookingRequest.java
+++ src/main/java/com/learn/jd/dto/BookingRequest.java
@@ -1,11 +1,14 @@
 package com.learn.jd.dto;
 
+import lombok.Data;
+
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Data
 public class BookingRequest {
 
     @NotNull
@@ -25,43 +28,4 @@ public class BookingRequest {
     @Min(1)
     private int numberOfGuests;
 
-    public String getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
-    }
-
-    public String getGuestId() {
-        return guestId;
-    }
-
-    public void setGuestId(String guestId) {
-        this.guestId = guestId;
-    }
-
-    public LocalDate getCheckInDate() {
-        return checkInDate;
-    }
-
-    public void setCheckInDate(LocalDate checkInDate) {
-        this.checkInDate = checkInDate;
-    }
-
-    public LocalDate getCheckOutDate() {
-        return checkOutDate;
-    }
-
-    public void setCheckOutDate(LocalDate checkOutDate) {
-        this.checkOutDate = checkOutDate;
-    }
-
-    public int getNumberOfGuests() {
-        return numberOfGuests;
-    }
-
-    public void setNumberOfGuests(int numberOfGuests) {
-        this.numberOfGuests = numberOfGuests;
-    }
 }
